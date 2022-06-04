855b077a1f518cf580cd2ca838c384ba0481f050
Stamen <xteam.stanly@gmail.com>
2022-06-04 20:34:13 +0200

    removed unused query

--- 3-nest.js/src/api/configuration/configuration.queries.ts
+++ 3-nest.js/src/api/configuration/configuration.queries.ts
@@ -11,19 +11,6 @@ const getAllConfigurationPrices: string = `
     INNER JOIN ram ON ram.id = "ramId"
     INNER JOIN storage ON storage.id = "storageId";`;
 
-const getConfigurationByName: string = `
-    SELECT
-        configuration.id,
-        configuration.name,
-        (cpu.price + gpu.price + motherboard.price + ram.price + storage.price) AS price
-    FROM
-        configuration
-    INNER JOIN cpu ON cpu.id = "cpuId"
-    INNER JOIN gpu ON gpu.id = "gpuId"
-    INNER JOIN motherboard ON motherboard.id = "motherboardId"
-    INNER JOIN ram ON ram.id = "ramId"
-    INNER JOIN storage ON storage.id = "storageId";`;
-
 export {
     getAllConfigurationPrices
 }
