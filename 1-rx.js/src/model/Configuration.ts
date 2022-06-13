e9d3dee6234766751c4815fd61fded081dac7056
Stamen <xteam.stanly@gmail.com>
2022-06-14 00:10:41 +0200

    fixed config calculations

--- 1-rx.js/src/model/Configuration.ts
+++ 1-rx.js/src/model/Configuration.ts
@@ -8,7 +8,7 @@ import { Storage } from "./Storage";
 // CPU - cores (25%, clamp 10), frequency (15%, clamp 3.7)
 // GPU - vram (25%, clamp 20)
 // RAM - capacity (15%, clamp 32), frequency (15%, clamp 3400)
-// STORAGE - capacity (5%, clamp 2)
+// STORAGE - capacity (5%, clamp 2048)
 // MOTHERBOARD - X
 // MAX SCORE 1000
 
@@ -29,11 +29,17 @@ export class Configuration {
     }
 
     public getPerforamanceMetric() : number {
-        const cpuMetrics: number = ~~clamp(map_range(this.cpu.cores, 0, 10, 0, 250) + map_range(this.cpu.frequency, 0, 3.7, 0, 67), 400);
-        const gpuMetrics: number = ~~clamp(map_range(this.gpu.vram, 0, 20, 0, 250), 250);
-        const ramMetrics: number = ~~clamp(map_range(this.ram.capacity, 0, 32, 0, 67) + map_range(this.ram.frequency, 0, 3400, 0, 67), 300);
-        const storageMetrics: number = ~~clamp(map_range(this.storage.capacity, 0, 2, 0, 50), 50);
-        const finalMetrics: number = ~~clamp(cpuMetrics + gpuMetrics + ramMetrics + storageMetrics, 1000);
+        const cores: number = ~~map_range(clamp(this.cpu.cores, 10), 0, 10, 0, 250);
+        const frequencyCpu: number = ~~map_range(clamp(this.cpu.frequency, 3.7), 0, 3.7, 0, 150);
+
+        const vram: number = ~~map_range(clamp(this.gpu.vram, 12), 0, 12, 0, 250);
+
+        const frequencyRam: number = ~~map_range(clamp(this.ram.frequency, 3400), 0, 3400, 0, 150);
+        const capacityRam: number = ~~map_range(clamp(this.ram.capacity, 32), 0, 32, 0, 150);
+
+        const capacityStorage: number = ~~map_range(clamp(this.storage.capacity, 2048), 0, 2048, 0, 50);
+
+        const finalMetrics: number = ~~clamp(cores + frequencyCpu + vram + frequencyRam + capacityRam + capacityStorage, 1000);
         return finalMetrics;
     }
 }
