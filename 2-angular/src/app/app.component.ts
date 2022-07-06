698b7334119198f40eb628fb6fa0d9d378a5da93
Stamen <xteam.stanly@gmail.com>
2022-07-06 20:35:46 +0200

    router qol changes

--- 2-angular/src/app/app.component.ts
+++ 2-angular/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router, RouterEvent, Event } from '@angular/router';
 import { fadeAnimation } from './animations';
+
+export enum Routes { Home = 0, Create = 1, About = 2 };
 
 @Component({
   selector: 'app-root',
@@ -7,6 +10,28 @@ import { fadeAnimation } from './animations';
   styleUrls: ['./app.component.scss'],
   animations: [fadeAnimation]
 })
-export class AppComponent {
-  title = '2-angular';
+export class AppComponent implements OnInit {
+  title = 'Dnevnik';
+  RoutesType = Routes;
+
+  constructor(private _router?: Router) {}
+
+  currentActiveRoute: Routes = Routes.Home;
+  activateRoute(route: Routes) : void {
+    this.currentActiveRoute = route;
+  }
+
+  ngOnInit(): void {
+    this._router?.events.subscribe((routerEvent: Event) => {
+      if(routerEvent instanceof NavigationEnd && routerEvent.url) {
+
+        switch(routerEvent.url.trim().toLowerCase().substring(1)) {
+          case 'new': this.currentActiveRoute = Routes.Create; break;
+          case 'about': this.currentActiveRoute = Routes.About; break;
+          default: this.currentActiveRoute = Routes.Home; break;
+        }
+
+      }
+    });
+  }
 }
